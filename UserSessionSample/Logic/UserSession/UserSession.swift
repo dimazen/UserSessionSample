33dbba6a53997121e93b7fd49dbfd1966c76e46b
zen <zen.dev@yandex.com>
2015-08-08 17:03:58 +0300

    Add UserSession implementation

--- UserSessionSample/Logic/UserSession/UserSession.swift
+++ UserSessionSample/Logic/UserSession/UserSession.swift
@@ -6,4 +6,62 @@
 import Foundation
 
 class UserSession {
+
+    let identifier: String // to uniquely identify session
+    private let sessionPrototype: UserSessionPrototype?
+
+    // MARK: - Init
+
+    init(prototype: UserSessionPrototype) {
+        identifier = prototype.identifier // any hashing function such as sha1 can be added here
+        sessionPrototype = prototype
+    }
+
+    init?(restorationIdentifier identifier: String) {
+        self.identifier = identifier
+        sessionPrototype = nil
+
+        let canRestore = true // check whether session for passed identifier can be successfuly restored or not
+        if !canRestore {
+            return nil
+        }
+    }
+
+    // MARK: - State
+
+    enum State {
+        case Undefined, Opened, Closed, Invalid
+    }
+
+    private(set) var state: State = .Undefined
+
+    func open() -> Bool {
+        precondition(state == .Undefined)
+
+        if let prototype = sessionPrototype where !bootstrapFromPrototype(prototype) {
+            state = .Invalid
+            return false
+        }
+
+        // point to start any related services
+
+        state = .Opened
+
+        return true
+    }
+
+    func close() {
+        precondition(state == .Opened || state == .Invalid)
+        // point to stop any related services
+
+        state = .Closed
+    }
+
+    // MARK: - Bootstrapping
+
+    private func bootstrapFromPrototype(prototype: UserSessionPrototype) -> Bool {
+        // map user data to a new user, setup db, etc
+
+        return true // bootstrap completed successfuly
+    }
 }
